932329f66129ec60f7c2274301992679bb499453
BlueAccords <myoldemailname@gmail.com>
2015-08-01 17:09:39 -0700

    Display user data in proper json format

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
+
   def home
+  	client_id= Rails.application.secrets[:soundcloud_client_key]
+	secret_id= Rails.application.secrets[:soundcloud_secret_key]
+  	base_uri = "http://api.soundcloud.com"
+  	@response = 
+  		HTTParty.get("#{base_uri}/users/2876232?client_id=#{client_id}")
+
   end
 end
