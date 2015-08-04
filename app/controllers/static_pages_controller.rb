ea03144c2ad755c6cb63637c147d519d8ba86319
BlueAccords <myoldemailname@gmail.com>
2015-08-03 18:16:53 -0700

    added basic test with omdb api

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,19 @@ class StaticPagesController < ApplicationController
   		HTTParty.get("#{base_uri}/users/2876232?client_id=#{client_id}")
 
   end
+
+  def omdb
+  	base_uri = "http://www.omdbapi.com/?"
+  	title = params[:title]
+  	year  = params[:year]
+		if !title.nil?
+			puts "TESTINGU"
+			@response = 
+				HTTParty.get("#{base_uri}t=#{title}&y=&plot=short&r=json")
+				# this line below works.
+				# HTTParty.get("http://www.omdbapi.com/?t=kingsman&y=&plot=short&r=json")
+
+			puts @response
+		end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home', to: 'static_pages#home'
-
+  post 'omdb', to: 'static_pages#omdb'
+  get 'omdb', to: 'static_pages#omdb'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
